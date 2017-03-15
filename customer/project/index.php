68de044d22aa99f0b79b949bd44fa464a100e378
Leeorichi <beatvn01@gmail.com>
2017-03-15 18:12:33 +0700

    Signed-off-by: Leeorichi <beatvn01@gmail.com>

--- customer/project/index.php
+++ customer/project/index.php
@@ -22,6 +22,7 @@
   <meta name="theme-color" content="#0cd563">
   <style>
     h1 {
+      /*
       text-shadow: 0 0 10px #fff,
       0 0 20px #fff,
       0 0 30px #fff,
@@ -29,7 +30,7 @@
       0 0 70px #ff00de,
       0 0 80px #ff00de,
       0 0 100px #ff00de,
-      0 0 150px #ff00de;
+      0 0 150px #ff00de;*/
       color: #8c8b8b;
     }
     hr{
