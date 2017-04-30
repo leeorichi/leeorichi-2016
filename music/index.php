7c5215dd39654aebf40e4e0ff792bbbda23d1c48
Leeorichi <beatvn01@gmail.com>
2017-04-30 08:54:37 +0700

    Update New Music Module, one sing, repeat one, ver 1.0
    Signed-off-by: Leeorichi <beatvn01@gmail.com>

---
+++ music/index.php
@@ -0,0 +1,22 @@
+﻿<!doctype html>
+<html class="no-js">
+
+<head>
+    <meta charset="utf-8">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+
+<body>
+    <div class="vz-wrapper">
+        <audio id="myAudio" src="music/NgauHung-Hoaprox-4120043.mp3" data-author="Leeeorichi" data-title="Lov3 - Ngau Hung"></audio>
+        <div class="vz-wrapper -canvas">
+            <canvas id="myCanvas" width="800" height="400"></canvas>
+        </div>
+    </div>
+    <script src="js/music.js"></script>
+</body>
+
+</html>
