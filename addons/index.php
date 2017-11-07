efae20c39b593fb6f1fe6f7f13e956a4f3bba0d6
Ha Van Long <beatvn01@gmail.com>
2017-11-07 15:49:36 +0700

    Signed-off-by: Ha Van Long <beatvn01@gmail.com>

--- addons/index.php
+++ addons/index.php
@@ -25,7 +25,7 @@
 				$this->wLog();
 				break;
 			}
-			echo "<center><img src='{$this->pic}' /></center>";
+			echo "<style>img { width: 100%; height: auto;} </style> <center><img src='{$this->pic}' /></center>";
 		}
 		
 		
